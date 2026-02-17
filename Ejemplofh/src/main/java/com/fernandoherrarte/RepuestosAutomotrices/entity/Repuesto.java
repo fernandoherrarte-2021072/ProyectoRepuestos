89c81cece9146f9af050fcef9e2891818e0d3995
fernandoherrarte-2021072 <fherrarte-2021072@gmail.com>
2026-02-17 10:25:45 -0600

    feat: Se creo el entity de Repuesto

---
+++ Ejemplofh/src/main/java/com/fernandoherrarte/RepuestosAutomotrices/entity/Repuesto.java
@@ -0,0 +1,78 @@
+package com.fernandoherrarte.RepuestosAutomotrices.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "Repuestos")
+public class Repuesto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    @Column(name = "nombre_repuesto")
+    private String nombreRepuesto;
+
+    @Column(name = "categoria_repuesto")
+    private String categoriaRepuesto;
+
+    @Column(name = "precio_compra")
+    private Double precioCompra;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @ManyToOne
+    @JoinColumn(name= "id_proveedor", nullable = false)
+    private Proveedor proveedor;
+
+    
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
+
+    public String getNombreRepuesto() {
+        return nombreRepuesto;
+    }
+
+    public void setNombreRepuesto(String nombreRepuesto) {
+        this.nombreRepuesto = nombreRepuesto;
+    }
+
+    public String getCategoriaRepuesto() {
+        return categoriaRepuesto;
+    }
+
+    public void setCategoriaRepuesto(String categoriaRepuesto) {
+        this.categoriaRepuesto = categoriaRepuesto;
+    }
+
+    public Double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(Double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+}
