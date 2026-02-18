36f0e99ab9ada0a40c5c70e5f92a75a9b6ae9d23
fernandoherrarte-2021072 <fherrarte-2021072@gmail.com>
2026-02-17 19:55:29 -0600

    Se agregO EL Service de Ventas

--- Ejemplofh/src/main/java/com/fernandoherrarte/RepuestosAutomotrices/service/VentaService.java
+++ Ejemplofh/src/main/java/com/fernandoherrarte/RepuestosAutomotrices/service/VentaService.java
@@ -1,14 +1,13 @@
-
 package com.fernandoherrarte.RepuestosAutomotrices.service;
 
-import com.fernandoherrarte.RepuestosAutomotrices.entity.Repuesto;
+import com.fernandoherrarte.RepuestosAutomotrices.entity.Venta;
 
 import java.util.List;
 
-public interface RepuestoService {
-    List<Repuesto> getAllRepuestos();
-    Repuesto getRepuestoById(Integer id);
-    Repuesto saveRepuesto(Repuesto repuesto) throws RuntimeException;
-    Repuesto uptadeRepuesto(Integer id, Repuesto repuesto);
-    void deleteRepuesto(Integer id);
+public interface VentaService {
+    List<Venta> getAllVentas();
+    Venta getVentaById(Integer id);
+    Venta saveVenta(Venta venta) throws RuntimeException;
+    Venta uptadeVenta(Integer id, Venta venta);
+    void deleteVenta(Integer id);
 }
